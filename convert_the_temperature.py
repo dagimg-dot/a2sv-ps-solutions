e0cb0ae6b87eaab25b4ff9b41d923749bcf2120e
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-03-23 22:19:34 +0300

    solve 07

---
+++ convert_the_temperature.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        fahrenheit = celsius * 1.80 + 32.00
+        kelvin = celsius + 273.15
+
+        return [kelvin, fahrenheit]
