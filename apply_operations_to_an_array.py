757dba728ff11a75824bbbf9b7c6bcc68ce8d8d8
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-03-27 22:51:22 +0300

    solve 14

---
+++ apply_operations_to_an_array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        l = 0
+
+        while l < len(nums) - 1:
+            if nums[l] == nums[l + 1]:
+                nums[l] *= 2
+                nums[l + 1] = 0
+                l += 2
+            else:
+                l += 1
+
+        z = nums.count(0)
+        nums = [i for i in nums if i != 0]
+        nums = nums + ([0] * z)
+
+        return nums
