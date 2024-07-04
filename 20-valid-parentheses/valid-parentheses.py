414236423254e480d4dd1735f0ac38156c3bd7d0
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-07-04 13:47:21 +0300

    Time: 38 ms (39.47%) | Memory: 16.6 MB (13.23%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        
+        stack = []
+
+        open = ['(', '[','{']
+        close = [')',']','}']
+        paren = ["()","[]","{}"]
+
+        for p in s:
+            if p in open:
+                stack.append(p)
+            else:
+                if len(stack) == 0:
+                    return False
+                elif (stack[-1] + p) in paren:
+                    stack.pop()
+                else:
+                    return False
+        
+        return not bool(len(stack))
+                
+        
