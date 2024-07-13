d8aa3439ed606789cc22c234a99ca34008d86851
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-07-13 11:23:18 +0300

    Time: 49 ms (85.26%) | Memory: 16.5 MB (43.52%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         i = 1
         while i < len(words):
-            if Counter(words[i]) == Counter(words[i-1]):
+            if sorted(words[i]) == sorted(words[i-1]):
                 words.pop(i)
             else:
                 i += 1
