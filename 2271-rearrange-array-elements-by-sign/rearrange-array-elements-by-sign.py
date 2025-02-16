624fbd1e5a93cd5ce4050b793b4531b7ca907b4f
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-02-16 23:58:13 +0300

    Time: 35 ms (93.31%) | Memory: 42.2 MB (56.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,15 +1,31 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos = [p for p in nums if p > 0]
-        neg = [n for n in nums if n < 0]
+        # pos = [p for p in nums if p > 0]
+        # neg = [n for n in nums if n < 0]
 
-        ans = []
+        # ans = []
 
-        for i in range(len(pos)):
-            ans.append(pos[i])
-            ans.append(neg[i])
+        # for i in range(len(pos)):
+        #     ans.append(pos[i])
+        #     ans.append(neg[i])
 
-        return ans
+        # # return ans
+
+        even_ptr = 0 
+        odd_ptr = 1 
+        
+        result = [0] * len(nums)
+        
+        for num in nums:
+            if num > 0:  
+                result[even_ptr] = num
+                even_ptr += 2
+            else:  
+                result[odd_ptr] = num
+                odd_ptr += 2  
+        
+        return result
+
 
         
 
