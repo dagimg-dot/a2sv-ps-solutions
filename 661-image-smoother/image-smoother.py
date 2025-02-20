62e609b0181ce105e762a141f9377579ded1fb4c
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-02-20 22:25:33 +0300

    Time: 180 ms (44.41%) | Memory: 18.8 MB (30.50%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -3,7 +3,14 @@ class Solution:
         m, n = len(img), len(img[0])
 
         def findNeighbour(i, j):
-            return [img[x][y] for x in (i-1, i, i+1) for y in (j-1, j, j+1) if 0 <= x < m and 0 <= y < n]
+            neighbors = [] 
+
+            for x in range(i-1, i+2):  
+                for y in range(j-1, j+2):
+                    if 0 <= x < m and 0 <= y < n:
+                        neighbors.append(img[x][y])
+            
+            return neighbors
 
 
         ans = [[0] * len(img[0]) for _ in range(m)]
