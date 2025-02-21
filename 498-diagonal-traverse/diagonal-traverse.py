3226e47c3e4519c44dc7efcf865af112e64df46f
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-02-22 02:03:15 +0300

    Time: 11 ms (65.79%) | Memory: 20.7 MB (30.30%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -2,17 +2,15 @@ class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         n, m = len(mat), len(mat[0])
 
-        diagonal_groups = {}
+        diagonal_sums = defaultdict(list)
 
         for i in range(n):
             for j in range(m):
-                key = i + j
-                if key not in diagonal_groups:
-                    diagonal_groups[key] = []
-                diagonal_groups[key].append(mat[i][j])
+                diagonal_sums[i+j].append(mat[i][j])
 
+        
         result = []
-        for diagonal_sum, elements in diagonal_groups.items():
+        for diagonal_sum, elements in diagonal_sums.items():
             if diagonal_sum % 2 == 0:
                 result.extend(elements[::-1])
             else:
