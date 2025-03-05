7f660717ef7794d5c466bd9a98c7c2cfaaf3680f
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-03-05 23:00:16 +0300

    Time: 2 ms (15.55%) | Memory: 17.7 MB (82.92%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,17 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def select(arr,i):
-            small = i
-            for j in range(i,len(arr)):
-                if arr[j] < arr[small]:
-                    small = j
-            return small
-        
-        for i in range(len(nums)):
-            smallest = select(nums,i)
-            nums[i], nums[smallest] = nums[smallest], nums[i]
-
         for i in range(len(nums)):
             small = i
             for j in range(i, len(nums)):
