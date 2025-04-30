542d492deac60ce3c498df6550eea3e49e570ed7
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-04-30 21:39:55 +0300

    Time: 115 ms (8.73%) | Memory: 18.9 MB (7.54%) - LeetSync

--- 372-super-pow/super-pow.py
+++ 372-super-pow/super-pow.py
@@ -1,11 +1,21 @@
 class Solution:
     def superPow(self, a: int, b: List[int]) -> int:
-        if a == 1:
-            return a
+        MOD = 1337
 
         n = int(''.join(map(str, b)))
 
-        return pow(a, n, 1337)
+        def pow(x, n):
+            if n == 1:
+                return x
+
+            pow2 = pow(x, n // 2) % MOD
+
+            if n % 2 == 0:
+                return pow2 * pow2 % MOD
+            else:
+                return x * pow2 * pow2 % MOD
+
+        return pow(a, n) % MOD
 
 
         
