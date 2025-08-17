95673a0076965402161f9bad8e16a4fd8698f41b
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-08-17 22:50:12 +0300

    Time: 120 ms (81.31%) | Memory: 18.1 MB (60.11%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [1] + [0] * amount
+
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] += dp[i - c]
+                
+        return dp[amount]
